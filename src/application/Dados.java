e49aef0d18dca1c91e64ba54e125a3da3124736a
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-08 19:02:15 -0300

    Initial Project

---
+++ src/application/Dados.java
@@ -0,0 +1,21 @@
+package application;
+
+
+public class Dados {
+
+	public static void main(String[] args) {
+		
+		double hp, lp, ha, la, qtd;
+		
+		hp = 2.6;
+		lp = 1.6;
+		ha = 0.35;
+		la = 0.3;
+		
+		qtd = (hp * lp) / (ha * la);
+		
+		System.out.println("Quantidade de azulejos necessarios para cobrir a parede:" + qtd);
+		}
+	}
+
+	
